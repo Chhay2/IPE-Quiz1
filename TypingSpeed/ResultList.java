02c222516e7429a8733859a74446aae9649566c2
VuthyChheangsrun <128031514+VuthyChheangsrun@users.noreply.github.com>
2023-03-24 15:13:10 +0700

    redo

--- TypingSpeed/ResultList.java
+++ TypingSpeed/ResultList.java
@@ -1,7 +1,7 @@
 package TypingSpeed;
 
 public class ResultList {
-    void Display(){
+    public static void main(String[] args) {
         System.out.println(
             """
             Test Result:
