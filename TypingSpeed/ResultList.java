2d9bb5cbb1bc7e8b894c0cfda8f6b06666e2ad1d
VuthyChheangsrun <128031514+VuthyChheangsrun@users.noreply.github.com>
2023-03-24 11:13:35 +0700

    list of test result for a user

---
+++ TypingSpeed/ResultList.java
@@ -0,0 +1,21 @@
+package TypingSpeed;
+
+public class ResultList {
+    void Display(){
+        System.out.println(
+            """
+            Test Result:
+                Subject                 Mark
+                OOP                     80
+                IPE                     81
+                Database                78
+                Automata                76
+                Web Design              84
+                Matlab                  70
+                Theory of Information   77
+            ----------------------------------
+                Average                 78
+            """
+        );
+    }
+}
