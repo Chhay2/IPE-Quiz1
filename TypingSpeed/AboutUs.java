cf486250c812696ccc34ce736d4cf3e5fc861764
Techchhiv <techchhivlim5@gmail.com>
2023-03-24 18:05:36 +0700

    Create AboutUs.java

---
+++ TypingSpeed/AboutUs.java
@@ -0,0 +1,25 @@
+package TypingSpeed;
+
+public class AboutUs {
+    public static void main(String[] args) {
+    System.out.println(""" 
+    
+    This a program that use for typing speed test and we have 6 member who constritibuted to this project.
+    This program have: 
+    - Login page
+    - Register page
+    - Reset password
+    - User typing test
+    - ResultList
+    ------------------------------------------
+    Our collaborator are:
+        1. Register Page          = San Vannak
+        2. Landing/Reset Password = Sam Jefferson
+        3. User Login             = Soeun Sopanharak
+        4. ResultList             = Vuthy Chheangsrun
+        5. User Typing Test       = Seng Enghav
+        6. About Us Page          = Lim Techchhiv
+    ------------------------------------------
+    """ );
+    }
+}
