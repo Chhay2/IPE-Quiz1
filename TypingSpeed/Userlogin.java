2b65b94307bcee9ee32f70b30e6530c9f6ca66e5
Sopanharak <128032308+Sopanharak@users.noreply.github.com>
2023-03-24 12:24:27 +0700

    Update Userlogin.java

--- TypingSpeed/Userlogin.java
+++ TypingSpeed/Userlogin.java
@@ -1,7 +1,32 @@
 package TypingSpeed;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Userlogin {
-    public static void main(String[] args) {
-        System.out.print("Hello vaannak");
+    HashMap<String, String> users;
+
+    public void Login(HashMap<String, String> users) {
+        this.users = users;
+    }
+
+    public void loginUser() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your username: ");
+        String username = input.nextLine();
+
+        System.out.print("Enter your password: ");
+        String password = input.nextLine();
+
+        if (users.containsKey(username)) {
+            if (users.get(username).equals(password)) {
+                System.out.println("Login successful!");
+            } else {
+                System.out.println("Incorrect password. Try again!");
+            }
+        } else {
+            System.out.println("User does not exist. Please register!");
+        }
     }
 }
