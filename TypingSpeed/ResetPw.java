b97d53035894584b6e09b5cc355f99a1988a8375
Chhay2 <semjefferson14@gmail.com>
2023-03-24 13:46:00 +0700

    Update ResetPw.java

--- TypingSpeed/ResetPw.java
+++ TypingSpeed/ResetPw.java
@@ -6,12 +6,13 @@ public class ResetPw {
     public static void main(String[] args) {
         System.out.println("Reset password");
         System.out.println("-------------------");
-        Scanner sc = new Scanner(System.in);
-        String Old, New; 
-        System.out.print("Insert old password:");
-        Old = sc.nextLine();
-        System.out.print("Insert new password:");
-        New = sc.nextLine();
+        try (Scanner sc = new Scanner(System.in)) {
+            String Old, New; 
+            System.out.print("Insert old password:");
+            Old = sc.nextLine();
+            System.out.print("Insert new password:");
+            New = sc.nextLine();
+        }
         System.out.println("The password is resetted");
     }
 }
