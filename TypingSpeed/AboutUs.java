d3719b130d3ac3e404c51716053751baa6d4775d
Techchhiv <127384271+Techchhiv@users.noreply.github.com>
2023-03-24 23:07:28 +0700

    Update AboutUs.java

--- TypingSpeed/AboutUs.java
+++ TypingSpeed/AboutUs.java
@@ -4,13 +4,14 @@ public class AboutUs {
     public static void main(String[] args) {
     System.out.println(""" 
     
-    This a program that use for typing speed test and we have 6 member who constritibuted to this project.
+    This a program that use for typing speed test and we have 7 member who contributed to this project.
     This program have: 
     - Login page
     - Register page
     - Reset password
     - User typing test
     - ResultList
+    - Read Menu
     ------------------------------------------
     Our collaborator are:
         1. Register Page          = San Vannak
@@ -19,6 +20,7 @@ public class AboutUs {
         4. ResultList             = Vuthy Chheangsrun
         5. User Typing Test       = Seng Enghav
         6. About Us Page          = Lim Techchhiv
+        7. Read Menu              = Pang Lyhour
     ------------------------------------------
     """ );
     }
